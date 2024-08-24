bee15031a01a060ff5c2046e797affe45b4acfb3
atharv-av <atharvv333@gmail.com>
2024-08-24 16:56:12 +0530

    added cross-env and dynamic build handling

--- index.js
+++ index.js
@@ -49,3 +49,4 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   connectDB();
 });
+
--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -1,15 +1,16 @@
 import jwt from "jsonwebtoken";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const generateJwt = (id, res) => {
   const token = jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "15d",
   });
-  res.cookie("token", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000,
-    secure: false,
-    httpOnly: false,
-    sameSite: "none",
-    path: "/"
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? 'none' : 'lax',
+    maxAge: 24 * 60 * 60 * 1000
   });
 };
 
